754fbdefbddba392cb17dd924e087cdd456f8139
Roy <55723741+roylyh@users.noreply.github.com>
2023-04-17 15:27:56 -0700

    Initialize 3-custom-hook

---
+++ src/pages/Info.js
@@ -0,0 +1,29 @@
+import Counter from 'Counter';
+
+export default function Info(props) {
+  const logout = props.logout;
+  const user = props.user;
+
+  // Show user Info
+  return (
+    <div>
+      <p className="UserInfo">
+        <div>You are logged in</div>
+        <div>Email: {user.email}</div>
+        <div>Name: {user.name}</div>
+        <div>UserId: {user.id}</div>
+      </p >
+      <p>
+        <button type="button" onClick={logout}>Logout</button>
+      </p>
+
+      <Counter
+        count={props.count}
+        increment={props.increment}
+        decrement={props.decrement}
+        clear={props.clear}
+      />
+      
+    </div>
+  );
+};
